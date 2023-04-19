792dec687623d0ccff724deee057e14a53c46407
lucas.avelarb <lucas.avelarb@hotmail.com>
2023-04-19 12:37:10 -0300

    feat: insert DbContext in DI

--- WebApiBestBuy.Infra/Data/AppDbContext.cs
+++ WebApiBestBuy.Infra/Data/AppDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using System.Data;
 using System.Data.Common;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiBestBuy.Infra.Data
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        public AppDbContext( ) {}
+        public AppDbContext( DbContextOptions<AppDbContext> options): base (options) {}
 
     }
 }
--- WebApiBestBuy/ExtensionService/ExtensionDI.cs
+++ WebApiBestBuy/ExtensionService/ExtensionDI.cs
@@ -5,6 +5,7 @@ using WebApiBestBuy.Infra.Data;
 using WebApiBestBuy.Domain.Services;
 using Microsoft.AspNetCore.Identity;
 using WebApiBestBuy.Infra;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiBestBuy.Api.ExtensionServices
 {
@@ -13,8 +14,8 @@ namespace WebApiBestBuy.Api.ExtensionServices
         public static IServiceCollection ConfigureServices(IServiceCollection Services, IConfiguration configuration)
         {
             Services.Configure<DatabaseConfig>(configuration.GetSection("ConnectionStrings"));
-      
 
+            Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(configuration["ConnectionStrings:ConnectionStringEscrita"]));
             Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
             Services.AddScoped<INotificationContext, NotificationContext>();
             Services.AddScoped<IProductRepository, ProductRepository>();
